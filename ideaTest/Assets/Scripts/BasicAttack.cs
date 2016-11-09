e816a77bb5aa489452e2da88697561fb5e3b5ee3
Sebastian Gil Parga <sebastian.gilp@fukl.edu>
2016-11-09 18:42:07 -0500

    Initial Commit with the finished instantiation of the attacks

---
+++ ideaTest/Assets/Scripts/BasicAttack.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAttack : MonoBehaviour {
+
+	public GameObject basicAttackPrefab;
+
+	public float xAttackOffset;
+	public float yAttackOffset;
+	public float zAttackOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject attack = InstantiateAttack();
+		}
+	}
+
+	private GameObject InstantiateAttack(){
+		Vector3 spawnPos = GetComponentInChildren<SpawnAttackArea>().getSpawnPosition();
+		GameObject attack = (GameObject)Instantiate (basicAttackPrefab,spawnPos, Quaternion.identity);
+		attack.transform.SetParent (transform);
+		return attack;
+	}
+}
